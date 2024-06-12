ffb3903878b9c093963a081e220ce23f7f135dbf
vladina21 <100123438+vladina21@users.noreply.github.com>
2024-06-13 01:02:10 +0300

    added include react to the optimizeDeps in vite.config

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -5,7 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    noDiscovery: true
+    noDiscovery: true,
+    include: ['react', 'react-dom']
   },
   // build: {
   //   rollupOptions: {
